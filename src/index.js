ebade79d30bf58d78ef3848642b28c62ac142d3c
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-03-26 19:37:14 +0100

    Initial pack with first class component in app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>;
+    }
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
